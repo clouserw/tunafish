e3e0b468a00b4f808db867513869ba7716732123
Austin King <shout@ozten.com>
2014-06-20 08:09:10 -0700

    Add pitch detection, basic styling

--- go.js
+++ go.js
@@ -1,7 +1,8 @@
-var useMicrophone = false;
+var useMicrophone = true;
 
 var audioCtx = new AudioContext;
 var analyser = audioCtx.createAnalyser();
+analyser.fftSize = 2048;
 
 if (useMicrophone) {
 
@@ -42,13 +43,11 @@ if (useMicrophone) {
   useSource(source, null);
 }
 
-
 function useSource(source, stream) {
   source.connect(analyser);
   analyser.connect(audioCtx.destination);
   visualize(stream);
 }
-
 
 function visualize(stream) {
   // Fake Audio from <audio>
@@ -62,27 +61,5 @@ function visualize(stream) {
 
 }
 
-// frameLooper() animates any style of graphics you wish to the audio frequency // Looping at the default frame rate that the browser provides(approx. 60 FPS)
+toggleLiveInput();
 
-var canvas = document.getElementById('viz');
-var ctx = canvas.getContext('2d');
-
-function frameLooper() {
-var canvas = document.getElementById('viz');
-var ctx = canvas.getContext('2d');
-
-  window.requestAnimationFrame(frameLooper);  
-  fbc_array = new Uint8Array(analyser.frequencyBinCount);
-  analyser.getByteFrequencyData(fbc_array);
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas 
-  ctx.fillStyle = '#00CCFF'; // Color of the bars
-  bars = fbc_array.length;
-
-  for (var i = 0; i < bars; i++) {
-    bar_x = i * 3;
-    bar_width = 2;
-    bar_height = -(fbc_array[i] / 2);
-    //fillRect( x, y, width, height ) // Explanation of the parameters below
-    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-  }
-}
